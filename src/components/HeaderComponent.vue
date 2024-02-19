0788c61c7ddc2cf265fc632b8068d404c2e31d10
Sagar Gurung <sgrgug@gmail.com>
2024-02-20 00:04:41 +0545

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+    <div class="flex justify-between items-center border-b-[1px] p-2">
+        <div class="bg-green-300">
+            <form action="" class="border-2">
+                <input type="text" name="" id="">
+            </form>
+        </div>
+        <div class="flex items-center">
+            <ion-icon name="notifications"></ion-icon>
+            <ion-icon name="person"></ion-icon>
+            <div class="flex items-center">
+                <div>Sagar</div>
+                <div>Admin</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="text-black py-2 text-sm">Monitor</div>
+    <div class="text-black py-2 text-sm">Heartbeats</div>
+    <div class="text-black py-2 text-sm">Incidents</div>
+    <div class="text-black py-2 text-sm">Team members</div>
+    <div class="text-black py-2 text-sm">Status pages</div>
+    <div class="text-black py-2 text-sm">Integrations</div>
+  </main>
+</template>
