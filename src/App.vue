e44303477df4828ce7b210fdc14eed15619d632e
Sagar Gurung <sgrgug@gmail.com>
2024-03-30 23:20:34 +0545

    static loading bar

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@ import SidebarComponent from "./components/SidebarComponent.vue";
 import { RouterView } from "vue-router";
 import { useSideBar } from "@/stores/sidebar";
 import { useModal } from "@/stores/modal";
+import { ref } from "vue";
 import { Icon } from "@iconify/vue";
 
 const { toggleModal } = storeToRefs(useModal());
 const { toggleSidebar } = storeToRefs(useSideBar());
+
+const loadingTimer = ref(true);
+setTimeout(() => {
+  loadingTimer.value = false;
+}, 7000);
 </script>
 
 <template>
@@ -23,19 +29,27 @@ const { toggleSidebar } = storeToRefs(useSideBar());
       <!-- End Close Div -->
 
       <div class="h-full w-[700px] overflow-hidden rounded-lg bg-white p-5">
-        <div class="flex h-[4%] items-center justify-between">
-          <div class="font-semibold">Payment Details</div>
-          <div class="cursor-pointer" @click="toggleModal = !toggleModal">
-            <Icon class="text-2xl" icon="charm:cross" />
-          </div>
+        <div
+          v-if="loadingTimer"
+          class="flex h-full items-center justify-center"
+        >
+          <img class="h-14 w-14" src="@/assets/loading.svg" alt="" />
         </div>
-        <div class="my-4 h-[96%] overflow-scroll">
-          <div v-for="index in 40" :key="index">
-            <div class="m-1 rounded-sm bg-gray-100 px-3 py-1">
-              <span class="uppercase text-stone-600"
-                >{{ index }}. Merchant:
-              </span>
-              <span>Developer Merchant</span>
+        <div v-else>
+          <div class="flex h-[4%] items-center justify-between">
+            <div class="font-semibold">Payment Details</div>
+            <div class="cursor-pointer" @click="toggleModal = !toggleModal">
+              <Icon class="text-2xl" icon="charm:cross" />
+            </div>
+          </div>
+          <div class="my-4 h-[96%] overflow-scroll">
+            <div v-for="index in 40" :key="index">
+              <div class="m-1 rounded-sm bg-gray-100 px-3 py-1">
+                <span class="uppercase text-stone-600"
+                  >{{ index }}. Merchant:
+                </span>
+                <span>Developer Merchant</span>
+              </div>
             </div>
           </div>
         </div>
